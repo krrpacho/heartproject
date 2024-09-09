7e316458ffcdb469c52a0bb7aa87aacf6817f20a
Пользователь <krrpacho@gmail.com>
2024-09-09 18:13:31 +0300

    space

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Canvas, useThree } from '@react-three/fiber';
+import { useLoader } from '@react-three/fiber';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { OrbitControls } from '@react-three/drei';
+
+
+function HeartModel() {
+  const gltf = useLoader(GLTFLoader, '/model/scene.gltf');
+
+  return <primitive object={gltf.scene} scale={3} />;
+}
+
+function CameraControls() {
+  const { camera } = useThree();
+
+  camera.position.set(0, 0, 5); 
+
+  return null;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: '100vh', width: '100vw',  backgroundColor: 'black' }}>
+      <Canvas>
+        <ambientLight intensity={0.9} />
+        <directionalLight position={[5, 5, 5]} intensity={10} />
+        <directionalLight position={[-5, -5, -5]} intensity={10} />
+        <pointLight position={[10, 10, 10]} intensity={10} />
+        <CameraControls />
+        <HeartModel />
+        <OrbitControls /> 
+      </Canvas>
     </div>
   );
 }
 
 export default App;
+
